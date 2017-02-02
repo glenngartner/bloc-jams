12510ada688c72cdeeeb93f7333b4e6c5bc21634
Glenn Gartner <glenngartner@gmail.com>
2017-02-01 22:55:15 -0500

    complete checkpoint 29

--- scripts/landing.js
+++ scripts/landing.js
@@ -2,50 +2,30 @@
  * Created by glenn on 12/28/2016.
  */
 
-var pointsArray = document.getElementsByClassName('point');
+var animatePoints = function () {
 
-var animatePoints = function (points) {
-
-    var revealFirstPoint = function () {
-        points[0].style.opacity = 1;
-        points[0].style.transform = "scaleX(1) translateY(0)";
-        points[0].style.msTransform = "scaleX(1) translateY(0)";
-        points[0].style.WebkitTransform = "scaleX(1) translateY(0)";
+    var revealPoint = function () {
+        $(this).css({
+            opacity: 1,
+            transform: 'scaleX(1) translateY(0)'
+        });
     };
 
-    var revealSecondPoint = function () {
-        points[1].style.opacity = 1;
-        points[1].style.transform = "scaleX(1) translateY(0)";
-        points[1].style.msTransform = "scaleX(1) translateY(0)";
-        points[1].style.WebkitTransform = "scaleX(1) translateY(0)";
-    };
-
-    var revealThirdPoint = function () {
-        points[2].style.opacity = 1;
-        points[2].style.transform = "scaleX(1) translateY(0)";
-        points[2].style.msTransform = "scaleX(1) translateY(0)";
-        points[2].style.WebkitTransform = "scaleX(1) translateY(0)";
-    };
-
-    revealFirstPoint();
-    revealSecondPoint();
-    revealThirdPoint();
+    $.each($('.point'), revealPoint);
 
 };
 
-window.onload = function () {
-    console.log(window.innerHeight);
+$(window).load(function () {
 
     // a window height of > 900 is better, for my desktop at least
-    if (window.innerHeight > 900) {
-        animatePoints(pointsArray);
+    if ($(window).height() > 900) {
+        animatePoints();
     }
-    var sellingPoints = document.getElementsByClassName('selling-points')[0];
-    var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+    var scrollDistance = $('.selling-points').offset().top - $(window).height() + 200;
 
-    window.addEventListener('scroll', function (e) {
-        if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
-            animatePoints(pointsArray);
+    $(window).scroll(function (e) {
+        if ($(window).scrollTop() >= scrollDistance) {
+            animatePoints();
         }
     })
-};
+});
